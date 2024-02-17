5d83d3c2254f64e59b714888f899a6bfd1964d12
Yoan15 <yoann.deco@gmail.com>
2024-02-17 22:01:39 +0100

    Fonction connection BDD

--- testCSharp/econtactClasses/contactClass.cs
+++ testCSharp/econtactClasses/contactClass.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,5 +20,33 @@ namespace testCSharp.econtactClasses
         public string Gender { get; set; }
 
         static string myconnstrng = ConfigurationManager.ConnectionStrings["connstrng"].ConnectionString;
+        
+        //Select from BDD
+        public DataTable Select() 
+        {
+            //connection BDD
+            SqlConnection conn = new SqlConnection(myconnstrng);
+            DataTable dt = new DataTable();
+            try
+            {
+                string sql = "SELECT * FROM tbl_contact";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                conn.Open();
+                adapter.Fill(dt);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return dt;
+        }
+
+        //Insert Data to BDD
+
     }
 }
