faea747ea1df28fa50bec7f34abe5b3cbe4d95ba
Yoan15 <yoann.deco@gmail.com>
2024-02-17 19:33:00 +0100

    Ajoutez des fichiers projet.

---
+++ testCSharp/Econtact.Designer.cs
@@ -0,0 +1,323 @@
+﻿
+namespace testCSharp
+{
+    partial class Econtact
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Econtact));
+            this.pictureBoxLogo = new System.Windows.Forms.PictureBox();
+            this.lblContactID = new System.Windows.Forms.Label();
+            this.txtboxContactID = new System.Windows.Forms.TextBox();
+            this.txtboxFirstName = new System.Windows.Forms.TextBox();
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.txtboxLastName = new System.Windows.Forms.TextBox();
+            this.lblbLastName = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.lblContactNumber = new System.Windows.Forms.Label();
+            this.txtboxAddress = new System.Windows.Forms.TextBox();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.cmbGender = new System.Windows.Forms.ComboBox();
+            this.lblGender = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lblDataGrid = new System.Windows.Forms.Label();
+            this.txtboxSearch = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxLogo
+            // 
+            this.pictureBoxLogo.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxLogo.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxLogo.Image")));
+            this.pictureBoxLogo.Location = new System.Drawing.Point(499, 12);
+            this.pictureBoxLogo.Name = "pictureBoxLogo";
+            this.pictureBoxLogo.Size = new System.Drawing.Size(314, 121);
+            this.pictureBoxLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxLogo.TabIndex = 0;
+            this.pictureBoxLogo.TabStop = false;
+            // 
+            // lblContactID
+            // 
+            this.lblContactID.AutoSize = true;
+            this.lblContactID.BackColor = System.Drawing.Color.Transparent;
+            this.lblContactID.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblContactID.Location = new System.Drawing.Point(12, 208);
+            this.lblContactID.Name = "lblContactID";
+            this.lblContactID.Size = new System.Drawing.Size(82, 21);
+            this.lblContactID.TabIndex = 1;
+            this.lblContactID.Text = "Contact ID";
+            this.lblContactID.Click += new System.EventHandler(this.lblContactID_Click);
+            // 
+            // txtboxContactID
+            // 
+            this.txtboxContactID.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtboxContactID.Location = new System.Drawing.Point(100, 205);
+            this.txtboxContactID.Name = "txtboxContactID";
+            this.txtboxContactID.Size = new System.Drawing.Size(244, 29);
+            this.txtboxContactID.TabIndex = 2;
+            // 
+            // txtboxFirstName
+            // 
+            this.txtboxFirstName.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtboxFirstName.Location = new System.Drawing.Point(100, 256);
+            this.txtboxFirstName.Name = "txtboxFirstName";
+            this.txtboxFirstName.Size = new System.Drawing.Size(244, 29);
+            this.txtboxFirstName.TabIndex = 4;
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.BackColor = System.Drawing.Color.Transparent;
+            this.lblFirstName.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblFirstName.Location = new System.Drawing.Point(12, 259);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(86, 21);
+            this.lblFirstName.TabIndex = 3;
+            this.lblFirstName.Text = "First Name";
+            // 
+            // txtboxLastName
+            // 
+            this.txtboxLastName.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtboxLastName.Location = new System.Drawing.Point(100, 304);
+            this.txtboxLastName.Name = "txtboxLastName";
+            this.txtboxLastName.Size = new System.Drawing.Size(244, 29);
+            this.txtboxLastName.TabIndex = 6;
+            // 
+            // lblbLastName
+            // 
+            this.lblbLastName.AutoSize = true;
+            this.lblbLastName.BackColor = System.Drawing.Color.Transparent;
+            this.lblbLastName.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblbLastName.Location = new System.Drawing.Point(12, 307);
+            this.lblbLastName.Name = "lblbLastName";
+            this.lblbLastName.Size = new System.Drawing.Size(84, 21);
+            this.lblbLastName.TabIndex = 5;
+            this.lblbLastName.Text = "Last Name";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox3.Location = new System.Drawing.Point(100, 351);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(244, 29);
+            this.textBox3.TabIndex = 8;
+            // 
+            // lblContactNumber
+            // 
+            this.lblContactNumber.AutoSize = true;
+            this.lblContactNumber.BackColor = System.Drawing.Color.Transparent;
+            this.lblContactNumber.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblContactNumber.Location = new System.Drawing.Point(12, 354);
+            this.lblContactNumber.Name = "lblContactNumber";
+            this.lblContactNumber.Size = new System.Drawing.Size(91, 21);
+            this.lblContactNumber.TabIndex = 7;
+            this.lblContactNumber.Text = "Contact No.";
+            // 
+            // txtboxAddress
+            // 
+            this.txtboxAddress.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtboxAddress.Location = new System.Drawing.Point(100, 401);
+            this.txtboxAddress.Multiline = true;
+            this.txtboxAddress.Name = "txtboxAddress";
+            this.txtboxAddress.Size = new System.Drawing.Size(244, 107);
+            this.txtboxAddress.TabIndex = 10;
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.BackColor = System.Drawing.Color.Transparent;
+            this.lblAddress.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblAddress.Location = new System.Drawing.Point(12, 404);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(66, 21);
+            this.lblAddress.TabIndex = 9;
+            this.lblAddress.Text = "Address";
+            // 
+            // cmbGender
+            // 
+            this.cmbGender.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.cmbGender.FormattingEnabled = true;
+            this.cmbGender.Items.AddRange(new object[] {
+            "Male",
+            "Female"});
+            this.cmbGender.Location = new System.Drawing.Point(100, 534);
+            this.cmbGender.Name = "cmbGender";
+            this.cmbGender.Size = new System.Drawing.Size(244, 29);
+            this.cmbGender.TabIndex = 11;
+            // 
+            // lblGender
+            // 
+            this.lblGender.AutoSize = true;
+            this.lblGender.BackColor = System.Drawing.Color.Transparent;
+            this.lblGender.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblGender.Location = new System.Drawing.Point(12, 536);
+            this.lblGender.Name = "lblGender";
+            this.lblGender.Size = new System.Drawing.Size(61, 21);
+            this.lblGender.TabIndex = 12;
+            this.lblGender.Text = "Gender";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.Transparent;
+            this.btnAdd.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnAdd.ForeColor = System.Drawing.Color.DarkGreen;
+            this.btnAdd.Location = new System.Drawing.Point(12, 627);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(156, 51);
+            this.btnAdd.TabIndex = 13;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.ForeColor = System.Drawing.Color.Orange;
+            this.button2.Location = new System.Drawing.Point(188, 627);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(156, 51);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Update";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.Transparent;
+            this.btnDelete.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnDelete.ForeColor = System.Drawing.Color.DarkRed;
+            this.btnDelete.Location = new System.Drawing.Point(364, 627);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(156, 51);
+            this.btnDelete.TabIndex = 15;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.Transparent;
+            this.btnClear.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnClear.ForeColor = System.Drawing.Color.Blue;
+            this.btnClear.Location = new System.Drawing.Point(545, 627);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(156, 51);
+            this.btnClear.TabIndex = 16;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(420, 256);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(823, 307);
+            this.dataGridView1.TabIndex = 17;
+            // 
+            // lblDataGrid
+            // 
+            this.lblDataGrid.AutoSize = true;
+            this.lblDataGrid.BackColor = System.Drawing.Color.Transparent;
+            this.lblDataGrid.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblDataGrid.Location = new System.Drawing.Point(420, 205);
+            this.lblDataGrid.Name = "lblDataGrid";
+            this.lblDataGrid.Size = new System.Drawing.Size(57, 21);
+            this.lblDataGrid.TabIndex = 18;
+            this.lblDataGrid.Text = "Search";
+            // 
+            // txtboxSearch
+            // 
+            this.txtboxSearch.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtboxSearch.Location = new System.Drawing.Point(484, 202);
+            this.txtboxSearch.Name = "txtboxSearch";
+            this.txtboxSearch.Size = new System.Drawing.Size(759, 29);
+            this.txtboxSearch.TabIndex = 19;
+            // 
+            // Econtact
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1307, 701);
+            this.Controls.Add(this.txtboxSearch);
+            this.Controls.Add(this.lblDataGrid);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lblGender);
+            this.Controls.Add(this.cmbGender);
+            this.Controls.Add(this.txtboxAddress);
+            this.Controls.Add(this.lblAddress);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.lblContactNumber);
+            this.Controls.Add(this.txtboxLastName);
+            this.Controls.Add(this.lblbLastName);
+            this.Controls.Add(this.txtboxFirstName);
+            this.Controls.Add(this.lblFirstName);
+            this.Controls.Add(this.txtboxContactID);
+            this.Controls.Add(this.lblContactID);
+            this.Controls.Add(this.pictureBoxLogo);
+            this.Name = "Econtact";
+            this.Text = "Econtact";
+            this.Load += new System.EventHandler(this.Econtact_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxLogo;
+        private System.Windows.Forms.Label lblContactID;
+        private System.Windows.Forms.TextBox txtboxContactID;
+        private System.Windows.Forms.TextBox txtboxFirstName;
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.TextBox txtboxLastName;
+        private System.Windows.Forms.Label lblbLastName;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label lblContactNumber;
+        private System.Windows.Forms.TextBox txtboxAddress;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.ComboBox cmbGender;
+        private System.Windows.Forms.Label lblGender;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lblDataGrid;
+        private System.Windows.Forms.TextBox txtboxSearch;
+    }
+}
+
